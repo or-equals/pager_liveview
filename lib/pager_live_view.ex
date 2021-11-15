e68ab096ad6922803632647335f8ced9a1b19a0c
Joshua Plicque <plicjo@gmail.com>
2021-11-15 00:53:13 -0500

    Simplify navigation links

--- lib/pager_live_view.ex
+++ lib/pager_live_view.ex
@@ -47,19 +47,13 @@ defmodule PagerLiveView do
 
   defp previous_link(route, results) do
     ~E"""
-    <svg class="mt-2 w-5 h-5 inline text-gray-700" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-      <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
-    </svg>
-    <%= live_redirect("Previous page", to: route.(results.prev_page), class: "relative inline-flex items-center py-2 text-sm font-medium rounded-md text-gray-700 bg-warm-gray-100 hover:underline") %>
+    <%= live_redirect("Previous", to: route.(results.prev_page), class: "relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50") %>
     """
   end
 
   defp next_link(route, results) do
     ~E"""
-    <%= live_redirect("Next page", to: route.(results.next_page), class: "ml-6 relative inline-flex items-center py-2 text-sm font-medium rounded-md text-gray-700 bg-warm-gray-100 hover:underline") %>
-    <svg class="mt-2 w-5 h-5 inline text-gray-700" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-      <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-    </svg>
+    <%= live_redirect("Next", to: route.(results.next_page), class: "ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50") %>
     """
   end
 end
